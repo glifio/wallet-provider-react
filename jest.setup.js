28025fab3a262a01bfdb330dcacbefa18cba71be
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-01-17 10:46:15 -0500

    Fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,10 @@ import '@testing-library/jest-dom'
 import 'jest-styled-components'
 import 'whatwg-fetch'
 
+import { TextDecoder, TextEncoder } from 'util'
+global.TextDecoder = TextDecoder
+global.TextEncoder = TextEncoder
+
 process.env.COIN_TYPE = 't'
 process.env.LOTUS_NODE_JSONRPC = 'https://calibration.node.glif.io/rpc/v0'
 process.env.FIL_SNAP_HOST = 'local:http://localhost:8081'
