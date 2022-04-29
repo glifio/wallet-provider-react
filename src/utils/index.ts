d377f4ec89827a80d2bc4b60d6783b182d82aafd
Thijs van Hoof <thijs.v.h@gmail.com>
2022-04-29 15:22:36 +0200

    Remove useGetMessage and useGetReplaceMessageGasParams

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
 export * from './ledger/reportLedgerConfigError'
 export * from './ledger/ledgerStateManagement'
-export * from './useGetMessage'
-export * from './useGetReplaceMessageGasParams'
 export * from './validateCID'
