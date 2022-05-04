09f842d2e900ec5e4305e3990e77e142059de923
Thijs van Hoof <thijs.v.h@gmail.com>
2022-05-04 19:25:13 +0200

    Remove Transaction utils and use proptypes from react-components (#129)

--- src/customPropTypes.ts
+++ src/customPropTypes.ts
@@ -1,30 +1,5 @@
 import { validateMnemonic } from 'bip39'
-import { CoinType, validateAddressString } from '@glif/filecoin-address'
-
-export const ADDRESS_PROPTYPE = (props, propName, componentName) => {
-  if (!validateAddressString(props[propName]))
-    return new Error(
-      `Invalid prop: ${propName} supplied to ${componentName}. Validation failed.`
-    )
-
-  return null
-}
-
-export const FILECOIN_NUMBER_PROP = (props, propName, componentName) => {
-  // instanceof prop checking is broken in nextjs on server side render cycles
-  const representsANum = Number.isNaN(Number(props[propName].toString()))
-  const hasFilecoinNumMethods = !!(
-    props[propName].toFil &&
-    props[propName].toAttoFil &&
-    props[propName].toPicoFil
-  )
-  if (!(representsANum || hasFilecoinNumMethods))
-    return new Error(
-      `Invalid prop: ${propName} supplied to ${componentName}. Validation failed.`
-    )
-
-  return null
-}
+import { CoinType } from '@glif/filecoin-address'
 
 export const MNEMONIC_PROPTYPE = (props, propName, componentName) => {
   if (!validateMnemonic(props[propName]))
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,2 @@
-export * from './getMaxGasFee'
 export * from './ledger/reportLedgerConfigError'
 export * from './ledger/ledgerStateManagement'
-export * from './useGetMessage'
-export * from './useGetReplaceMessageGasParams'
-export * from './validateCID'
